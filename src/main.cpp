83ca6ec0e49f4a82a1369db0eed69a0337d728c4
Weebifying <weebifylmao123@gmail.com>
2024-07-20 09:19:40 +0700

    just to make sure

--- src/main.cpp
+++ src/main.cpp
@@ -41,15 +41,17 @@ class $modify(AltOptionsLayer, OptionsLayer) {
         auto menu = this->m_mainLayer->getChildByID("options-menu");
 
         menu->getChildByID("options-button")->setPositionX(menu->getChildByID("account-button")->getPositionX());
-        auto videoButton = CCMenuItemSpriteExtra::create(
-            ButtonSprite::create("Graphics", 130, 0, 1.0, true, "goldFont.fnt", "GJ_button_01.png", 0.0),
-            this,
-            menu_selector(AltOptionsLayer::onVideo)
-        );
-        videoButton->setContentSize(menu->getChildByID("options-button")->getContentSize());
-        videoButton->setPosition({menu->getChildByID("how-to-play-button")->getPositionX(), menu->getChildByID("options-button")->getPositionY()});
-        videoButton->setID("graphics-button");
-        menu->addChild(videoButton);
+        if (!menu->getChildByID("graphics-button")) {
+            auto videoButton = CCMenuItemSpriteExtra::create(
+                ButtonSprite::create("Graphics", 130, 0, 1.0, true, "goldFont.fnt", "GJ_button_01.png", 0.0),
+                this,
+                menu_selector(AltOptionsLayer::onVideo)
+            );
+            videoButton->setContentSize(menu->getChildByID("options-button")->getContentSize());
+            videoButton->setPosition({menu->getChildByID("how-to-play-button")->getPositionX(), menu->getChildByID("options-button")->getPositionY()});
+            videoButton->setID("graphics-button");
+            menu->addChild(videoButton);
+        }
     }
 
     void onVideo(CCObject* sender) {
