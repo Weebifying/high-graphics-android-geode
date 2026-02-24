33b4839668ba5bb4bfa278da75423518bd9f54b6
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2026-02-24 14:37:14 -0500

    Add comment

--- src/hooks/CCFileUtils.cpp
+++ src/hooks/CCFileUtils.cpp
@@ -5,6 +5,9 @@
 using namespace geode::prelude;
 
 class $modify(CCFileUtils) {
+    // When the texture quality is changed, the game needs to purge the CCFileUtils cache to load the new textures.
+    // For some inexplicable reason, the cache isn't purged when this function isn't hooked, even though the function is called in the game's code.
+    // This is a workaround to ensure the cache is purged when changing graphics settings.
     static void purgeFileUtils() {
         CCFileUtils::purgeFileUtils();
     }
